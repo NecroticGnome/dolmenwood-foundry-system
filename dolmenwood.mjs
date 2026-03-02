dbe8c5ec04448ea3bc8c7b4582b1fd894edbd91f
Blaze Sanecki <sanecki.blazej@gmail.com>
2026-03-02 02:08:40 +0100

    Fix encumbrance data not updating when switching methods
    Changing the encumbrance method setting now calls prepareData() on all
    adventurer actors before re-rendering sheets, so prepareDerivedData()
    recomputes system.final and system.encumbranceResult with the new method.

--- dolmenwood.mjs
+++ dolmenwood.mjs
@@ -228,7 +228,8 @@ Hooks.once('init', async function () {
 			foundry.applications.instances?.forEach(app => {
 				if (app.collection?.documentName === 'Item') app.render()
 			})
-			// Re-render open adventurer sheets
+			// Recompute derived data and re-render open adventurer sheets
+			game.actors.filter(a => a.type === 'Adventurer').forEach(a => a.prepareData())
 			Object.values(ui.windows).forEach(app => {
 				if (app.document?.type === 'Adventurer') app.render()
 			})
